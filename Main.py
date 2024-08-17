313e810dda0b726326227f79718ba449bd173600
Trnrr <20446954+Trnrr@users.noreply.github.com>
2024-08-17 16:03:50 -0500

    remove trickshotting servers

--- Main.py
+++ Main.py
@@ -1,16 +1,19 @@
 import json
+import re
 import requests
 from bs4 import BeautifulSoup
 
 url = "https://master.iw4.zip/servers"
 server_list = []
+removeWords = ["trickshot","s&r"]
 
 html = BeautifulSoup(requests.get(url).text, 'html.parser')
 
 h2m_servers = html.find('div', {"id":"H2M_servers"}).findAll('table')[0]
 
 for tr in h2m_servers.findAll('tr', {"class":"server-row"}):
-    server_list.append(tr.get('data-ip') +":"+ tr.get('data-port'))
+    if not re.search('trickshot|s&r', tr.find('td', {"class":"server-hostname text-break"}).get('data-hostname'), re.IGNORECASE):
+        server_list.append(tr.get('data-ip') +":"+ tr.get('data-port'))
 
 with open("favourites.json", "w") as outfile:
     outfile.write(json.dumps(server_list, indent=2))
